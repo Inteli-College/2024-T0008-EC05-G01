24a9b57d7720be84d5397d0635498340978dc998
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-02-27 22:35:21 -0300

    continuinado userflow CLI

--- src/main.py
+++ src/main.py
@@ -1,31 +1,29 @@
-from serial.tools import list_ports
 from classes.robot import RobotWrapper
 from classes.kit import KitLoader
 from colorama import Fore, Style
 import inquirer
-import pydobot
 import yaspin
 import typer
 import os
 
 from commands.moveto import app as moveto_app
 
+spinner = yaspin(text="Processando...", color="red")
 app = typer.Typer()
 app.add_typer(moveto_app)
 
 @app.command()
 def interface():
     robot = RobotWrapper()
+    choices=["atuador_on", "movimentar", "montar_kit", "posição_atual", "sair"]
     while True:
-        choices=["home", "atuador_on", "movimentar", "posição_atual" "sair"]
-
         perguntas_main = [
             inquirer.List("interface", message="Qual comando você quer executar ?", choices=choices),
         ]
 
         respostas_main = inquirer.prompt(perguntas_main)
 
-        processar(respostas_main, robot, choices)
+        choices = processar(respostas_main, robot, choices)
 
 @app.command()
 def whereami():
@@ -33,7 +31,6 @@ def whereami():
     curr_pos = robot.current()
 
     print(f"[{Fore.YELLOW}ROBOT{Style.RESET_ALL}] Eu estou em X: {Fore.MAGENTA}{curr_pos['x']}{Style.RESET_ALL}, Y: {Fore.MAGENTA}{curr_pos['y']}{Style.RESET_ALL}, Z: {Fore.MAGENTA}{curr_pos['z']}{Style.RESET_ALL}")
-
 
 @app.command('execute-kit')
 def execute_kit():
@@ -52,7 +49,6 @@ def execute_kit():
     kit_loader.execute_kit(robot)
     print(f"[{Fore.YELLOW}APP{Style.RESET_ALL}] Kit {Fore.GREEN}{kit_name}{Style.RESET_ALL} executado com sucesso!")
 
-
 def processar(dados, robot, choices):
     comando = dados["interface"]
 
@@ -61,31 +57,51 @@ def processar(dados, robot, choices):
     if robot.inicalazed == False:
         robot.init()
 
-    if comando == "atuador_on":
-        robot.atuador_on()
-
-        if "atuador_off" not in choices:
-            choices.append("atuador_off")
-
     match comando:
-        case "home":
+        case "back_home":
+            spinner.start()
             robot.move(-10, 235, 140)
+            spinner.stop()
+
+        case "atuador_on":
+            spinner.start()
+            robot.atuador_on()
+            spinner.stop()
+
+            if "atuador_on" in choices:
+                choices.remove("atuador_on")
+                choices.insert(0, "atuador_off")
         
         case "atuador_off":
+            spinner.start()
             robot.atuador_off()
+            spinner.stop()
 
             if "atuador_off" in choices:
                 choices.remove("atuador_off")
+                choices.insert(0, "atuador_on")
         
         case "movimentar":
             x = float(typer.prompt("Digite a distância a ser movida no eixo X:"))
             y = float(typer.prompt("Digite a distância a ser movida no eixo Y:"))
             z = float(typer.prompt("Digite a distância a ser movida no eixo Z:"))
+
+            if "back_home" not in choices:
+                choices.insert(4, "back_home")
             
-            robot.move(x,y,z) # perguntar ao gustavo qual é a função correta de movimentar
+            spinner.start()
+            robot.move(x,y,z) 
+            spinner.stop()
 
         case "posição_atual":	
             print(robot.current())
+
+        case "montar_kit":
+            spinner.start()
+            execute_kit()
+            spinner.start()
+
+    return choices
 
 if __name__ == "__main__":
     app()
