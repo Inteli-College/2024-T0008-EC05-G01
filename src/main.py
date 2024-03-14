d206446bf469ec1c5d0fe711b23645cf013c7c7c
ifferreira <ivan.ferreira@sou.inteli.edu.br>
2024-03-14 18:53:09 -0300

    feat: redefinir posicoes dos medicamentos

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from commands.atual import app as atual
 from commands.ferramenta import app as ferramenta
 from commands.home import app as home
 from commands.montar_kit import app as montar_kit
+from commands.redefinir_pos_kit import app as redefinir_pos_kit
 
 app = typer.Typer()
 
@@ -15,6 +16,7 @@ app.registered_commands += atual.registered_commands
 app.registered_commands += ferramenta.registered_commands
 app.registered_commands += montar_kit.registered_commands
 app.registered_commands += home.registered_commands
+app.registered_commands += redefinir_pos_kit.registered_commands
 
 
 if __name__ == "__main__":
