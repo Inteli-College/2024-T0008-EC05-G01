8d568d4aa3cabd0393ac809b6e934fc78a631e17
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-04-11 10:53:51 -0300

    Update

--- src/novo_backend/static/scripts/fila.js
+++ src/novo_backend/static/scripts/fila.js
@@ -1,4 +1,4 @@
-function adicionar(){
+function adicionar() {
     var fila = [];
     while (true) {
         var nome = prompt("Digite o nome do kit que deseja executar ou clique em 'Cancelar' para parar: ");
@@ -10,25 +10,34 @@ function adicionar(){
 
     console.log(fila);
 
-    var dados = {
-        fila: fila
-    };
+    var index = 0;
 
-    fetch('/fila', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(dados),
-    })
-    .then(async response => {
-        if (!response.ok) {
-            throw new Error('Erro ao adicionar kit na fila');
-        }
-        if (response.ok) {
-            alert("Kit adicionado com sucesso!");
-            console.log('Kit adicionado com sucesso:', dados);
+    function enviarProximoKit() {
+        if (index < fila.length) { 
+            var kitAtual = fila[index];
+            fetch(`/fila/${kitAtual}`, { 
+                method: 'GET', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(async response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao adicionar kit na fila');
+                }
+                if (response.ok) {
+                    alert("Kit adicionado com sucesso!");
+                    console.log('Kit adicionado com sucesso:', kitAtual);
+                    index++; 
+                    enviarProximoKit(); 
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao adicionar kit na fila:', error);
+            });
+        } else {
             window.location.href = "/auxiliar";
         }
-    })
+    }
+    enviarProximoKit();
 }
